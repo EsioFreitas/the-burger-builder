708d7864d77fb86ac5180fdb35877c1ad0ba67d5
EsioFreitas <esio.gustavo@gmail.com>
2019-02-08 14:04:58 -0200

    Conect state to build controls

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import BuiltControl from './BuildControl/BuildControl';
+
 import './BuildControls.css';
 
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Meat', type: 'meat'},
+    { label: 'Cheese', type: 'cheese'},
+]
 const buildControls = props =>(
-    <div>
-
+    <div className='BuildControls'>
+        {
+            controls.map(ctrl =>(
+                <BuiltControl key={ctrl.label} 
+                              label={ctrl.label} 
+                              addIngredients={()=>props.addIgredients(ctrl.type)}
+                              removeIgredients={()=>props.removeIgredients(ctrl.type)}/>
+            ))
+        }
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,8 +1,16 @@
 import React, {Component, Fragment} from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuilderControls from '../../components/Burger/BuildControls/BuildControls';
 
 import './BurgerBuilder.css';
+
+const INGREDIENTS_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.2,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
     state = {
@@ -11,15 +19,43 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+    
     render(){
         return(
             <Fragment>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Builder Controls</div>
+                <BuilderControls addIgredients={this.addIgredients}
+                                 removeIgredients={this.removeIgredients}/>
             </Fragment>
         );
+    }
+
+    addIgredients = (type) =>{
+        const aldCont = this.state.ingredients[type];
+        const newCont = aldCont+1;
+        const updateIngredients = {...this.state.ingredients};
+        updateIngredients[type] = newCont;
+
+        const priceAdd = INGREDIENTS_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAdd;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+
+    removeIgredients = (type) =>{
+        const oldCont = this.state.addIgredients[type];
+        const newCont = oldCont - 1;
+        const updateIgredients = {...this.state.ingredients};
+        updateIgredients[type] = newCont;
+
+        const priceAdd = INGREDIENTS_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAdd;
+
+        this.setState({totalPrice: newPrice, ingredients: updateIgredients});
     }
 }
 
