cd715287e0b8b49f9c5d3b93a0abc2c869019298
EsioFreitas <esio.gustavo@gmail.com>
2019-02-14 17:09:58 -0200

    Add inputs

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://my-burger-9364b.firebaseio.com'
+    baseURL: 'https://my-burger-9364b.firebaseio.com/'
 });
 
 export default instance;
